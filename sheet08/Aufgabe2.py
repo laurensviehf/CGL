4afce6c31e05c53d15db86f3b98ded2ecf8f4153
Felix_Laptop <st188157@stud.uni-stuttgart.de>
2025-12-13 17:29:55 +0100

    schau mal

--- sheet08/Aufgabe2.py
+++ sheet08/Aufgabe2.py
@@ -6,6 +6,13 @@ A = 1.0
 gamma = 0.2
 omega = 2 * np.pi
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--gamma', type=float, default=0.2, help='Dämpfungskoeffizient')
+parser.add_argument('--A', type=float, default=1.0, help='Amplitude der Schwingung')
+parser.add_argument('--omega', type=float, default=2 * np.pi, help='Kreisfrequenz der Schwingung')
+args = parser.parse_args()
+
+
 t = np.linspace(0, 10, 500)
 
 x = A * np.exp(-gamma * t) * np.cos(omega * t)
